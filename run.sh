4e1bffddd8c57bb9110e5d22d7bfa73b5ba3dd2b
Ammar Arjomand <Persiancookbook@gmail.com>
2024-10-23 14:29:47 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@ resetColour=`echo -e '\033[0m'`
 colours=('\033[1;31m' '\033[1;32m' '\033[1;33m' '\033[1;34m' '\033[1;35m' '\033[1;36m' '\033[1;37m' '\033[1;38m' '\033[1;39m' '\033[0;31m' '\033[0;32m' '\033[2;33m' '\033[2;34m' '\033[2;35m' '\033[2;36m' '\033[2;37m' '\033[2;38m' '\033[2;39m')
 
 function prepend_output () {
-    local streamTitle=$1
-    local streamColour=$2
-    streamTitle=$(awk '{x=$0;for(i=length;i<40;i++)x=x " ";}END{print x}' <<< "$streamTitle")
-    sed -e "s/^/${streamColour}${streamTitle}${resetColour}| /"
+  local streamTitle=$1
+  local streamColour=$2
+  streamTitle=$(awk '{x=$0;for(i=length;i<40;i++)x=x "_";}END{print x}' <<< "$streamTitle")
+  sed -e "s/^/${streamColour}${streamTitle}${resetColour}/"
 }
 
 function getRepoList() {
-    az repos list --org 'https://dev.azure.com/ajgre' --project "$PROJECT" --query [].[name,sshUrl] --output tsv
+  az repos list --org 'https://dev.azure.com/ajgre' --project "$PROJECT" --query [].[name,sshUrl] --output tsv
 }
 
 function cloneRepos() {
@@ -21,10 +21,8 @@ function cloneRepos() {
     local streamColour=`echo -e "${colours[RANDOM%${#colours[@]}]}"`
     local name=$(echo $line | awk '{print $1}')
     local sshUrl=$(echo $line | awk '{print $2}')
-    git clone $sshUrl | prepend_output $name $streamColour &
-    echo $sshUrl | prepend_output $name $streamColour &
+    git clone $sshUrl 2>&1 | prepend_output $name $streamColour &
   done
-  wait
 }
 
 function menu(){
@@ -59,3 +57,4 @@ function menu(){
 }
 
 menu
+wait
